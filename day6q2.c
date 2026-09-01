e590877fbc5066a5e8bfad7d1d0dc88c43ad5b77
Taiwan Deori <rmdaoshi@gmail.com>
2026-09-02 00:38:29 +0530

    Add files via upload

---
+++ day6q2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int num;
+
+	printf("Enter an integer: ");
+	scanf("%d", &num);
+
+	if (num >= 0) {
+		if (num == 0) {
+			printf("The number is zero\n");
+		} else {
+			printf("%d is positive\n", num);
+		}
+	} else {
+		printf("%d is negative\n", num);
+	}
+
+	return 0;
+}
