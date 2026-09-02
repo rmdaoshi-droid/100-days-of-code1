772ba4d2cf7aa73f0ec2f4325f5f5db13fa0fc50
Taiwan Deori <rmdaoshi@gmail.com>
2026-09-02 12:22:04 +0530

    Add files via upload

---
+++ day7q1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int year;
+
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if (year % 400 == 0)
+    {
+        printf("The year is a leap year.");
+    }
+    else if (year % 100 == 0)
+    {
+        printf("The year is not a leap year.");
+    }
+    else if (year % 4 == 0)
+    {
+        printf("The year is a leap year.");
+    }
+    else
+    {
+        printf("The year is not a leap year.");
+    }
+
+    return 0;
+}
