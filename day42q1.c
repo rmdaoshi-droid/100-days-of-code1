896b964646272fad6a87dfc2f86baa455917f22d
Taiwan Deori <rmdaoshi@gmail.com>
2026-09-20 00:41:01 +0530

    Add files via upload

---
+++ day42q1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0') {
+        char ch = str[i];
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            vowels++;
+        }
+        else if ((ch >= 'a' && ch <= 'z') ||
+                 (ch >= 'A' && ch <= 'Z')) {
+            consonants++;
+        }
+
+        i++;
+    }
+
+    printf("Vowels=%d, Consonants=%d", vowels, consonants);
+
+    return 0;
+}
